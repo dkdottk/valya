284e45ca3274b0876e143d6676f4315b14ea1317
Valentyn <valantg@gmail.com>
2021-09-24 16:38:28 +0300

    fix

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ const items = makesGalleryItem(galleryItems);
 gallery.append(...items);
 
 gallery.addEventListener('click', onClickHandler);
+
 document
   .querySelector('[data-action="close-lightbox"]')
   .addEventListener('click', onCloseHandler);
+
 document
   .querySelector('.lightbox__overlay')
   .addEventListener('click', onCloseHandler);
+
 document.addEventListener('keydown', onPushEsc);
